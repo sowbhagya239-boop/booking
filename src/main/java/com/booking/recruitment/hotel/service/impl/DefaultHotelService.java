ae29de0f4eee4f20ce5f94ded6cb1b9251c0fdd4
sowbhagya239-boop <sowbhagya239@gmail.com>
2026-03-03 23:05:57 +0530

    Implement hotel deletion and retrieval methods

--- src/main/java/com/booking/recruitment/hotel/service/impl/DefaultHotelService.java
+++ src/main/java/com/booking/recruitment/hotel/service/impl/DefaultHotelService.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 @Service
 class DefaultHotelService implements HotelService {
   private final HotelRepository hotelRepository;
+  private static final EARTH_RADIUS = 6371; //km
 
   @Autowired
   DefaultHotelService(HotelRepository hotelRepository) {
@@ -39,4 +40,62 @@ class DefaultHotelService implements HotelService {
 
     return hotelRepository.save(hotel);
   }
+
+  @Override
+  public boolean deleteHotel(Long id) {
+    Hotel hotel = hotelRepository.findById(id);
+
+    if(hotel == null || hotel.isDeleted()) {
+      return false;
+      }
+    hotel.setDeleted(true);
+    hotelRepository.save(hotel);
+    return true;
+  }
+
+  @Override
+  public Hotel getHotelById(Long id) {
+    Hotel hotel = hotelRepository.findById(id);
+
+    if(hotel == null || hotel.isDeleted()) {
+      return null;
+      }
+    return hotel;
+  }
+
+  @Override
+  public List<Hotel> findTop3ClosestHotels(Long cityId) {
+    List<Hotel> hotels = hotelRespository.findByCityId(cityId);
+
+    if(hotels == null) {
+      return Collections.emptyList();
+    }
+    City city = hotels.get(0).getCity();
+    double cityLat = city.getCityCentreLatitude();
+    double cityLon = city.getCityCentreLongitude();
+
+    return hotels.stream()
+      .filter(h -> !h.isDeleted())
+      .sorted(Comparator.comparingDouble(h -> getCalculateDistance(cityLat, cityLon,h.getLatitude(),h.getLongitude())))
+      .limit(3)
+      .collect(Collectors.toList());
+    
+  }
+  private double getCalculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    double latDistance = Math.toRadians(lat2 - lat1);
+    double lonDistance = Math.toRadians(lon2 - lon1);
+
+    double a = Math.sin(latDistance/2) * Math.sin(latDistance /2 ) 
+      + Math.cos(Math.toRadians(lat1))
+      * Math.cos(Math.toRadians(lat2))
+      * Math.sin(lonDistance/2)
+      * Math.sin(lonDistance/2);
+
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return EARTH_RADIUS *c;
+  }
+
+  
+
+  
 }
