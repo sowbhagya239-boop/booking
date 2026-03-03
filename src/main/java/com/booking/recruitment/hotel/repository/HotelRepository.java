ccb80ba51f4b50d4401dd2d638654b0d331644cd
sowbhagya239-boop <sowbhagya239@gmail.com>
2026-03-03 23:15:52 +0530

    Add method to find hotels by city ID

--- src/main/java/com/booking/recruitment/hotel/repository/HotelRepository.java
+++ src/main/java/com/booking/recruitment/hotel/repository/HotelRepository.java
@@ -3,4 +3,6 @@ package com.booking.recruitment.hotel.repository;
 import com.booking.recruitment.hotel.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HotelRepository extends JpaRepository<Hotel, Long> {}
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+  List<Hotel> findByCityId(Long cityId);
+}
